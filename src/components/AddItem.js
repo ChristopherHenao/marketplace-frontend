f1402eaee8cd69ee3e5211856ffd5b6ef187d42e
sb7297 <sbaireddy7297@gmail.com>
2021-07-26 18:28:14 -0400

    AddItem: build static form for adding items

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -3,8 +3,44 @@ import React from 'react'
 const AddItem = () => {
 
     return (
-        <div>
-            Add Item
+        <div id="addItemFormContainer">
+            <form id="addItemForm">
+                <div id="itemNameDiv">
+                    <label>Item name
+                        <input name="item_name" type="text"></input>
+                    </label>
+                </div>
+
+                <div id="itemPriceDiv">
+                    <label>Item price
+                        <input name="item_price" type="text"></input>
+                    </label>
+                </div>
+
+                <div id="itemCategoryDiv">
+                    <label>Item category
+                        <select name="item_category">
+                            <option>Animal Products</option>
+                            <option>Beans</option>
+                            <option>Cereals - Maize</option>
+                            <option>Cereals - Rice</option>
+                            <option>Cereals - Other</option>
+                            <option>Fruits</option>
+                            <option>Peas</option>
+                            <option>Roots & Tubers</option>
+                            <option>Seeds & Nuts</option>
+                            <option>Vegetables</option>
+                            <option>Other</option>
+                        </select>
+                    </label>
+                </div>
+
+                <div id="itemDescriptionDiv">
+                    <label>Item description
+                        <textarea name="item_description"></textarea>
+                    </label>
+                </div>
+            </form>
         </div>
     )
 }
