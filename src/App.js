1e1aae1bb7ce70345e02c712f9c3ad9d6be51c28
ChristoherHenao <cq.henao@gmail.com>
2021-07-26 18:17:35 -0400

    connected to api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react'
 import ItemList from './components/ItemList';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      App
+      <NavBar/>
       <ItemList />
     </div>
   );
