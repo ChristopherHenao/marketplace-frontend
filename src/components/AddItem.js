719cfd5447c20f558c278a1552aed05612588525
sb7297 <sbaireddy7297@gmail.com>
2021-07-28 13:12:16 -0400

    use flexbox to style AddItem form

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -6,39 +6,44 @@ const AddItem = () => {
         <div id="addItemFormContainer">
             <form id="addItemForm">
                 <div id="itemNameDiv">
-                    <label>Item name
-                        <input name="item_name" type="text"></input>
-                    </label>
+                    <label>Item name</label>
+                    <input
+                        name="item_name"
+                        type="text"
+                        placeholder="Item name" />
                 </div>
 
                 <div id="itemPriceDiv">
-                    <label>Item price
-                        <input name="item_price" type="text"></input>
-                    </label>
+                    <label>Item price</label>
+                    <input
+                        name="item_price"
+                        type="text"
+                        placeholder="Item price" />
                 </div>
 
                 <div id="itemCategoryDiv">
-                    <label>Item category
-                        <select name="item_category">
-                            <option>Animal Products</option>
-                            <option>Beans</option>
-                            <option>Cereals - Maize</option>
-                            <option>Cereals - Rice</option>
-                            <option>Cereals - Other</option>
-                            <option>Fruits</option>
-                            <option>Peas</option>
-                            <option>Roots & Tubers</option>
-                            <option>Seeds & Nuts</option>
-                            <option>Vegetables</option>
-                            <option>Other</option>
-                        </select>
-                    </label>
+                    <label>Item category</label>
+                    <select name="item_category">
+                        <option>Animal Products</option>
+                        <option>Beans</option>
+                        <option>Cereals - Maize</option>
+                        <option>Cereals - Rice</option>
+                        <option>Cereals - Other</option>
+                        <option>Fruits</option>
+                        <option>Peas</option>
+                        <option>Roots & Tubers</option>
+                        <option>Seeds & Nuts</option>
+                        <option>Vegetables</option>
+                        <option>Other</option>
+                    </select>
                 </div>
 
                 <div id="itemDescriptionDiv">
-                    <label>Item description
-                        <textarea name="item_description"></textarea>
-                    </label>
+                    <label>Item description</label>
+                    <textarea
+                        placeholder="Item description"
+                        name="item_description">
+                    </textarea>
                 </div>
             </form>
         </div>
